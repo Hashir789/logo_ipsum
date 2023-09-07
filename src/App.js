390440d7be7e4c0d5edf1bcb928f60060b49fc8e
Hashir789 <m.hashir.malik1020@gmail.com>
2023-09-07 09:30:31 +0500

    Made Advisor Top Bar Responsive

--- src/App.js
+++ src/App.js
@@ -3,15 +3,17 @@ import { BrowserRouter as Router, Routes } from 'react-router-dom';
 import AdvisorSidebar from './components/AdvisorSidebar';
 import { Box } from '@mui/material';
 import AState from './context/AState';
-// import AdvisorTopBar from './components/AdvisorTopBar';
+import AdvisorTopBar from './components/AdvisorTopBar';
 
 const App = () => {
   return (
     <>
     <AState>
-      <Box sx={{display: 'flex', flexDirection: 'row'}}>
+      <Box sx={{display: 'flex', flexDirection: 'row', boxSizing: 'border-box'}}>
         <AdvisorSidebar/>
-        {/* <AdvisorTopBar/> */}
+        <Box sx={{display: 'flex', flexDirection: 'column'}}>
+          <AdvisorTopBar/>
+        </Box>
         <Router>
           <Routes>
           </Routes>
--- src/components/AdvisorTopBar.js
+++ src/components/AdvisorTopBar.js
@@ -10,23 +10,23 @@ const AdvisorTopBar = () => {
     console.log(active)
   return (
     <Box>
-        <Box sx={{ width: '1455px', height: '84px', marginTop: '36px', display: 'flex', alignItems: 'center', justifyContent: 'space-between' }} mx={5}>
-            <Box sx={{ disply: 'flex', alignItems: 'space-between', justifyContent: 'space-between', flexDirection: 'column', color: '#27364E', height: '100%', width: '100%' }}>
-                <Box sx={{ height: '48px' }}><Typography variant='h4' sx={{ fontWeight: 600, fontSize: '32px', lineHeight: '48px' }}>{(sideBarList.find(item => item.text === active) || { subText: '' }).text}</Typography></Box>
-                <Box sx={{ width: '288px', height: '24px', marginTop: '12px' }}><Typography variant='h4' sx={{ fontWeight: 400, fontSize: '16px', lineHeight: '24px' }}>{(sideBarList.find(item => item.text === active) || { subText: '' }).subText}</Typography></Box>
+        <Box sx={{ width: '80.5vw', height: '4vw', display: 'flex', alignItems: 'center', justifyContent: 'space-between', boxSizing: 'border-box'}} px={{xl: 6, lg: 3, sm: 2}} mt={{xl: 4, lg: 3, sm: 2}}>
+            <Box sx={{ disply: 'flex', alignItems: 'space-between', justifyContent: 'space-between', flexDirection: 'column', color: '#27364E', width: '40vw'}}>
+                <Box><Typography variant='h4' sx={{ fontWeight: 600, fontSize: '1.75vw' }}>{(sideBarList.find(item => item.text === active) || { subText: '' }).text}</Typography></Box>
+                <Box sx={{ marginTop: '1vw' }}><Typography variant='h4' sx={{ fontWeight: 400, fontSize: '1vw' }}>{(sideBarList.find(item => item.text === active) || { subText: '' }).subText}</Typography></Box>
             </Box>
-            <Box sx={{ display: 'flex', flexDirection: 'row', width: '794px', height: '76px' }}>
+            <Box sx={{ display: 'flex', flexDirection: 'row', width: '50vw', height: '7vh', alignItems: 'center', justifyContent: 'space-between' }}>
                 <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center', position: 'relative' }}>
-                    <input type="text" placeholder="Search here" style={{width: '550px', height: '70px', fontSize: '16px', borderRadius: '18px', padding: '2px 0px 2px 32px', border: 'none', backgroundColor: '#FFF6EF'}} />
-                    <Box sx={{ position: 'absolute', right: '32px' }}>
-                        <SearchIcon style={{ color: '#27364E' }}/>
+                    <input type="text" placeholder="Search here" style={{width: '29vw', height: '4vw', fontSize: '1vw', borderRadius: '0.75vw', padding: '0vw 0vw 0vw 1.5vw', border: 'none', backgroundColor: '#FFF6EF'}} />
+                    <Box sx={{ position: 'absolute', right: '1.5vw' }}>
+                        <SearchIcon style={{ color: '#27364E', fontSize: '1.5vw' }}/>
                     </Box>
                 </Box>
-                <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center', width: '74px', height: '74px', borderRadius: '22px', border: '1px solid #C4C4C4', position: 'relative' }} ml='20px'>
-                    <TbMessageDots style={{ width: '28px', height: '28px' }}/>
-                    <Box sx={{ position: 'absolute', width: '34px', height: '34px', borderRadius: '9px', border: '3px solid #F3F2F7', bgcolor: '#FFBB54', top: -4, right: -4, display: 'flex', alignItems: 'center', justifyContent: 'center', color: 'white', fontWeight: 700 }}>84</Box>
+                <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center', width: '4vw', height: '4vw', borderRadius: '1vw', border: '0.1vw solid #C4C4C4', position: 'relative' }}>
+                    <TbMessageDots style={{ width: '1.5vw', height: '1.5vw' }}/>
+                    <Box sx={{ position: 'absolute', width: '1.7vw', height: '1.7vw', borderRadius: '0.4vw', border: '0.2vw solid #F3F2F7', bgcolor: '#FFBB54', top: -6, right: -6, display: 'flex', alignItems: 'center', justifyContent: 'center', color: 'white', fontWeight: 700, fontSize: '0.75vw' }}>84</Box>
                 </Box>
-                <Box sx={{ width: '74px', height: '74px', borderRadius: '22px', bgcolor: '#C4C4C4' }} ml='20px'></Box>
+                <Box sx={{ width: '4vw', height: '4vw', borderRadius: '1vw', bgcolor: '#C4C4C4' }}></Box>
             </Box>
         </Box>
     </Box>
